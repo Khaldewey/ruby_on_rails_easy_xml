1ad36bd0264bc3aab28e35b0f48e39da804ef1f0
Israel <israel_alves77@hotmail.com>
2024-08-14 19:02:41 -0300

    hotfix: correção da página e rotas de edição de senha para membros externos

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,3 +1,20 @@
 class Admin::UsersController < Admin::ResourceController
-  
+  def edit_password
+    @user = current_user
+  end
+
+  def update_password
+    @user = User.find(current_user.id)
+    if @user.update(user_params)
+      sign_in @user, :bypass => true
+      redirect_to admin_root_path
+    else
+      render :edit_password
+    end
+  end 
+
+  private
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
 end
