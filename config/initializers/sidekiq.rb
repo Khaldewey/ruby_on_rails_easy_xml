582316327a6af83617bb87da709180f52d5bd23b
Israel Alves <israel_alves77@hotmail.com>
2024-08-18 01:28:18 -0300

    hotfix: correção na view index dos documents

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,13 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { 
+    url: 'rediss://default:AeoUAAIjcDFhZDkzNjc3N2JjNmQ0ZTFiOTdmODlmYTA5ODUzZjE1N3AxMA@model-tortoise-59924.upstash.io:6379',
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { 
+    url: 'rediss://default:AeoUAAIjcDFhZDkzNjc3N2JjNmQ0ZTFiOTdmODlmYTA5ODUzZjE1N3AxMA@model-tortoise-59924.upstash.io:6379',
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
 end
